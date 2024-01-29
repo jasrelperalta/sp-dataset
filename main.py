c9c46c233870a33c8294a548d8122312b084cd01
Jasrel Peralta <jasrelp.matchhome@gmail.com>
2024-01-29 18:06:01 +0800

    Added authentication and create session

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
-import requests
+import requests, os
+from dotenv import load_dotenv
 
-url = "https://api.themoviedb.org/3/authentication"
+load_dotenv("secret.env")
 
-headers = {"accept": "application/json"}
+def authenticate():
+    url = "https://api.themoviedb.org/3/authentication"
 
-response = requests.get(url, headers=headers)
+    headers = {"accept": "application/json",
+            "Authorization": f"Bearer {os.getenv('TMDB_API_READ')}"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        print("Authentication successful")
+    else:
+        print("Authentication failed")
+        print(response.json().get("status_message"))
+
+def create_session():
+    url = "https://api.themoviedb.org/3/authentication/token/new"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {os.getenv('TMDB_API_READ')}"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print(response.text)
+
+    if response.status_code == 200:
+        print("Session created")
+        return response.json().get("request_token")
+    else:
+        print("Session creation failed")
+        print(response.json().get("status_message"))
+
+def get_movielist():
+    url = "https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=action"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {os.getenv('TMDB_API_READ')}"
+    }
