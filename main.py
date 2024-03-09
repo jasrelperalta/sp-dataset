37bfbc4e7ae55663773d3ba905a8185f99df5aef
jasrelperalta <jtperalta1@up.edu.ph>
2024-03-09 21:35:28 +0800

    fix error handling and counters

--- main.py
+++ main.py
@@ -43,14 +43,7 @@ def create_session():
 # for loop for getting the movie details for each genre
 def get_movie_details(genre_ids, genre_names):
     for i in range(len(genre_ids)):
-        counter = 0
-        movielist = get_movielist(genre_ids[i])
-
-        # create a folder to store the movie list
-        if not os.path.exists("movielists"):
-            os.makedirs("movielists")
-
-        print(f"Downloading posters and writing movie list for {genre_names[i]} with {len(movielist)} movies")
+        counter = 0 # counter for the number of movies downloaded
 
         # create a folder to store the posters
         if not os.path.exists("posters"):
@@ -60,20 +53,23 @@ def get_movie_details(genre_ids, genre_names):
         if not os.path.exists(f"posters/{genre_names[i]}"):
             os.makedirs(f"posters/{genre_names[i]}")
 
-        for movie in movielist:
-            # store the movie id, title, poster path, release date in a file")
-            if download_poster(movie['movie_poster'], movie['movie_id'], movie['movie_title'], genre_names[i], counter, 0):
-                with open("movielists/master.out", "a") as file:
-                    file.write(f"{movie['movie_id']} - {movie['movie_title']} - {movie['movie_poster']} - {genre_names[i]}\n")
-                    counter += 1
+        # get the movie list for each genre
+        movielist = get_movielist(genre_ids[i], genre_names[i])
 
+        # create a folder to store the movie list
+        if not os.path.exists("movielists"):
+            os.makedirs("movielists")
+
+        print(f"Downloading posters and writing movie list for {genre_names[i]} with {len(movielist)} movies")
+
+        for movie in movielist:
             with open(f"movielists/{genre_names[i]}.out", "a") as file:
                 file.write(f"{movie['movie_id']} - {movie['movie_title']} - {movie['movie_poster']}\n")
 
         print(f"Done downloading posters and writing movie list for {genre_names[i]}")
 
 # gets the movie list for each genre
-def get_movielist(genre_id):
+def get_movielist(genre_id, genre_name):
     counter = 0
     page_list = []
     movie_list = []
@@ -89,16 +85,18 @@ def get_movielist(genre_id):
         response = requests.get(url, headers=headers)
 
         if response.status_code == 200:
-            print(f"Movie list retrieved for genre {genre_id} page {page_number} counter {counter}")
+            print(f"Movie list retrieved for genre {genre_name} page {page_number} counter {counter}")
             for movie in response.json().get("results"):
                 if movie.get("poster_path") is None:
                     print(f"Movie {movie.get('id')} - {movie.get('title')} has no poster")
-
                 else:
-                    movie_list.append({"movie_id": movie["id"], "movie_title": movie["title"], "movie_poster": movie["poster_path"]})
-                    counter += 1
-                    if counter == 2000:
-                        break
+                    if download_poster(movie["poster_path"], movie["id"], movie["title"], genre_name, counter, 0):
+                        movie_list.append({"movie_id": movie["id"], "movie_title": movie["title"], "movie_poster": movie["poster_path"]})
+                        counter += 1
+                        if counter == 2000:
+                            break
+                    else:
+                        print(f"Movie {movie.get('id')} - {movie.get('title')} has broken link poster")
 
             page_list.append(page_number)
             page_number = get_random_page_number(page_list)
@@ -113,22 +111,34 @@ def get_random_page_number(page_list):
     while True:
         page_number = random.randint(1, 500)
         if page_number not in page_list:
-            return page_number 
+            return page_number
 
-# count the number of movies in the object
-def count_movies_in_list_response(response):
-    counter = 0
-    for movie in response.get("results"):
-        if movie.get("poster_path") is not None:
-            counter += 1
-        else:
-            print(f"Movie {movie.get('id')} - {movie.get('title')} has no poster: Not Counted")
-    return counter
+# check if the poster is available and does not return error 403
+def check_poster(poster_path):
+    url = f"https://image.tmdb.org/t/p/original{poster_path}"
 
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {os.getenv('TMDB_API_READ')}"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+    
 
 # download the poster of the movie
 def download_poster(poster_path, movie_id, movie_title, genre_name, counter, retry_count):
     # download the poster to the movie id for easy reference
+    
+    if check_poster(poster_path):
+        pass
+    else:
+        print(f"Poster for movie {movie_id} - {movie_title} is not available")
+        return False
 
     print(f"{counter}/2000  Downloading poster for movie {movie_id} - {movie_title}")
     url = f"https://image.tmdb.org/t/p/original{poster_path}"
@@ -141,22 +151,27 @@ def download_poster(poster_path, movie_id, movie_title, genre_name, counter, ret
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        with open(f"posters/{genre_name}/{movie_id}.jpg", "wb") as file:
+        with open(f"posters/{genre_name}/{counter}_{movie_id}.jpg", "wb") as file:
             file.write(response.content)
         return True
     
-    else:
-        print(f"Failed to download poster for movie {movie_id}")
-        download_poster(poster_path, movie_id, movie_title, genre_name, counter, retry_count + 1)
-
     if retry_count == 5:
         print(f"Failed to download poster for movie {movie_id} after 5 retries")
         # ask retry again
         retry = input("Retry again? (y/n): ")
         if retry == "y":
-            download_poster(poster_path, movie_id, movie_title, genre_name, counter, 0)
+            if(download_poster(poster_path, movie_id, movie_title, genre_name, counter, 0)):
+                return True
+            else:
+                return False
         else:
             return False
+        
+    else:
+        print(f"Failed to download poster for movie {movie_id}")
+        if(download_poster(poster_path, movie_id, movie_title, genre_name, counter, retry_count + 1)):
+            return True
+
     
         
 
